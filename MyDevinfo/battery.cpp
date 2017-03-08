170e3d670062285be46992ea88bd390d0ae75990
E841719 <hjgode@gmail.com>
2017-03-08 09:24:56 +0100

    changed return   return spex.BackupBatteryLifePercent; to   DEBUGMSG(1, (L"+++ BatteryLifePercent=%i\n", spex.BatteryLifePercent));   return spex.BatteryLifePercent; Verified to run on 99ex and does update RSSI and read BatteryPercent.

--- MyDevinfo/battery.cpp
+++ MyDevinfo/battery.cpp
@@ -5,7 +5,9 @@ int getBatteryPercent(){
 	memset(&spex, 0, sizeof(SYSTEM_POWER_STATUS_EX));	
 	BOOL bRes = GetSystemPowerStatusEx(&spex, TRUE);
 	if(bRes){
-		return spex.BackupBatteryLifePercent;
+		//return spex.BackupBatteryLifePercent;
+		DEBUGMSG(1, (L"+++ BatteryLifePercent=%i\n", spex.BatteryLifePercent));
+		return spex.BatteryLifePercent;
 	}
 	else{
 		DWORD dwRes=GetLastError();
