6dfb2154848309be4e51ef78c56bc50f15d649fb
E841719 <hjgode@gmail.com>
2018-05-15 16:28:54 +0200

    cosmetic change

--- MyDevinfo/getLocalIP.cpp
+++ MyDevinfo/getLocalIP.cpp
@@ -16,14 +16,17 @@ char* my_getLocalIP()
     struct hostent *host_entry;
     host_entry=gethostbyname(szHostName);
     char * _szLocalIP;
-    _szLocalIP = inet_ntoa (*(struct in_addr *)*host_entry->h_addr_list);
-	strncpy(szLocalIP, _szLocalIP, 16);
-
-    MultiByteToWideChar(CP_ACP, 0,_szLocalIP , -1,pWideCharStr , sizeof(pWideCharStr) / sizeof(wchar_t));
-
-    //MultiByteToWideChar(CP_UTF8, MB_COMPOSITE,szLocalIP , 32, pWideCharStr,32);
-    //MessageBox(NULL,pWideCharStr, TEXT("IP address"), MB_OK|MB_ICONERROR);
-	DEBUGMSG(1, (L"getLocalIP='%s'", pWideCharStr));
+	if(host_entry!=NULL){
+		_szLocalIP = inet_ntoa (*(struct in_addr *)*host_entry->h_addr_list);
+		strncpy(szLocalIP, _szLocalIP, 16);
+		MultiByteToWideChar(CP_ACP, 0,_szLocalIP , -1,pWideCharStr , sizeof(pWideCharStr) / sizeof(wchar_t));
+		DEBUGMSG(1, (L"getLocalIP='%s'", pWideCharStr));
+		DEBUGMSG(1, (L"h_length=%i\n",host_entry->h_length)); //The length, in bytes, of each address.
+	}
+	else{
+		DEBUGMSG(1, (L"gethostbyname error=%i\nlocalIP set to 0.0.0.0\n", WSAGetLastError()));
+		printf(szLocalIP, "0.0.0.0");
+	}
     WSACleanup();
     return szLocalIP;
 }
